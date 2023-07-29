8556e8c393f024b8a955995db1de1a505d88cc91
Mathew Wang <mathewnxwang@gmail.com>
2023-07-28 20:44:07 -0700

    refactored conversation.py

--- mechanics/conversation.py
+++ mechanics/conversation.py
@@ -6,7 +6,8 @@ from mechanics.turn import player_turn
 def conversation_round(
     round_counter: int,
     players: Dict[str, str],
-    conversation: str
+    conversation: str,
+    thoughts: list
     ) -> int:
     '''
     Generate one round of conversation where every player speaks once
@@ -20,14 +21,12 @@ def conversation_round(
         
     # Every player contributes to the conversation once in order
     for player_id, player_type in players.items():        
-        player_turn(
-            player_id,
-            player_type,
-            conversation,
-            prompt_template)
+        conversation, thoughts = player_turn(
+            player_id, player_type, prompt_template, conversation, thoughts
+        )
     
     round_counter += 1
-    return round_counter
+    return round_counter, conversation, thoughts
 
 def conversation_full(
     rounds: int,

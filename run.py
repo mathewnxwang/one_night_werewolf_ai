8b38cda5d40ea0a9fa59121e0634c6717e810f5a
Mathew Wang <mathewnxwang@gmail.com>
2024-01-01 13:00:24 -0800

    updated game manager

--- run.py
+++ run.py
@@ -3,25 +3,14 @@ from typing import List
 import streamlit as st
 
 from mechanics.players import PlayerManager
-from mechanics.vote import show_results
-from mechanics.conversation import conversation_full
+from mechanics.game_manager import GameManager
 from mechanics.utils.role_actions import RoleActions
 
 def full_game(rounds: int):
     '''
     Execute full conversation and vote
     '''
-
-    # initialize variables
-    conversation = ''
-    thoughts = []
-
-    player_manager = PlayerManager(players_n=5)
-    player_data = player_manager.construct_player_data()
-    enriched_players = RoleActions().execute_all_actions(player_data)
-    st.write(enriched_players)
-
     st.markdown('#### Deliberation')
-    conversation, thoughts = conversation_full(rounds, enriched_players, conversation, thoughts)
-
-    show_results(enriched_players, conversation)
+    game_manager = GameManager()
+    game_manager.conversation_full(rounds)
+    game_manager.show_results()
